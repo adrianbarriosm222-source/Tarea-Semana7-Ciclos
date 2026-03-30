44bd488c1aa707e2fc00cd7e48ae604856b6a44f
adrianbarrios <adrianbarriosm222@gmail.com>
2026-03-29 18:34:53 -0600

    Cambios de codigo

---
+++ ejercicio1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Datos correctos
+    string usuario_correcto = "admin";
+    int contrasena_correcta = 2026;
+
+    // Variables
+    string usuario;
+    int contrasena;
+    int intentos_fallidos = 0;
+
+    // Ciclo while
+    while (usuario != usuario_correcto || contrasena != contrasena_correcta) {
+        cout << "Ingrese el nombre de usuario: ";
+        cin >> usuario;
+
+        cout << "Ingrese la contraseña numerica: ";
+        cin >> contrasena;
+
+        // Validación
+        if (usuario != usuario_correcto || contrasena != contrasena_correcta) {
+            cout << "Datos incorrectos. Intente nuevamente.\n\n";
+            intentos_fallidos++;
+        }
+    }
+
+    // Mensajes finales
+    cout << "\nBienvenido al sistema.\n";
+    cout << "Acceso concedido.\n";
+    cout << "Intentos fallidos: " << intentos_fallidos << endl;
+    Cout <<"Rodrigo Adrian Barrios Monteroso" ;
+    return 0;
+}
